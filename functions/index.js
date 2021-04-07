49bc5ae6838492eeb0cceeec5db8d3aebec6cae7
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-04-07 11:46:18 -0700

    Rename vars to be more specific

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-onst functions = require("firebase-functions");
+const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 
 // Switch Database to ahs-app
@@ -25,22 +25,24 @@ exports.checkPendingNotifs = functions.pubsub.schedule('every 10 minutes').onRun
 			const dataBaseOps = [];
 			// Loop through each child
 			snapshot.forEach(child => {
-				console.log(child.key);
-				// childData - hold the values of the notif (can access properties ex. childData.notifTimestamp)
-				const childData = child.val()
-				console.log(child.key + ": " + "timestamp: " + childData.notifTimestamp)
+
+				const articleID = child.key
+				const notif = child.val()
+				console.log(`notification "${articleID}" will be sent at ${notif.notifTimestamp}`)
 				
 				// If the notifTimestamp is in the past, then swap data and send notif        
-				if (Number(childData.notifTimestamp) < currentTime) return false
+				if (notif.notifTimestamp < currentTime) return false
 				
-				console.log("Transfer Ready " + child.key);
+				console.log(`sending ${articleID}...`);
 				// Change notif timestamp to time of push
 				childData.notifTimestamp = currentTime
 				// Swap Data
-				dataBaseOps.push(database.ref('pendingNotifs/' + child.key).remove())
-				dataBaseOps.push(database.ref('notifications/' + child.key).set(childData))
+				dataBaseOps.push(
+					database.ref('pendingNotifs/' + articleID).remove(),
+					database.ref('notifications/' + articleID).set(notif)
+				)
 				// Send Notif
-				sendNotif(childData, child.key);
+				sendNotif(notif, articleID);
 			})
 			// Execute all the queued database operations
 			Promise.all(dataBaseOps);
@@ -48,20 +50,18 @@ exports.checkPendingNotifs = functions.pubsub.schedule('every 10 minutes').onRun
 
 })
 
-function sendNotif(articleData, key) {
+function sendNotif(notif, articleID) {
 	// Construct the message
 	const message = {
 		notification: {
-			title: articleData.title,
-			body: articleData.blurb
+			title: notif.title,
+			body: notif.blurb
 		},
-		data: {
-			articleID: key
-		},
+		data: { articleID },
 		to: '/topics/' + articleData.categoryID
 	};
 	// Convert message into string
-	const body = JSON.stringify(message);
+	const body = JSON.stringify(message)
 	// Post request with fetch()
 	const result = fetch('https://fcm.googleapis.com/fcm/send', {
 		method: 'POST',
@@ -79,13 +79,12 @@ function sendNotif(articleData, key) {
 
 exports.incrementViews = functions.https.onCall((data, context) => {
   const articleID = data.id;
-  admin.database().ref('/articles/'+articleID+'/views').once('value', 
+  return admin.database().ref('/articles/'+articleID+'/views').once('value', 
     snapshot =>{
       // List of database operations (acts like a queue)
       const dataBaseOps = [];
       dataBaseOps.push(admin.database().ref('/articles/'+articleID+'/views').set(Number(snapshot.val()+1)));
       // Execute all the queued database operations
       Promise.all(dataBaseOps);
-      return {status: "Good"};
     });
 });
